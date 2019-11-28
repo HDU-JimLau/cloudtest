13a9a9c0fc1fff48bed152e249ba62cb497a8e73
JimLau <438951386@qq.com>
2019-11-28 11:00:54 +0800

    add annotation

--- consumer_service/src/main/java/com/jim/ConsumerServiceApplication.java
+++ consumer_service/src/main/java/com/jim/ConsumerServiceApplication.java
@@ -16,8 +16,7 @@ import org.springframework.web.client.RestTemplate;
 public class ConsumerServiceApplication {
 
 	public static void main(String[] args) {
-		//test
-		//test2
+
 		SpringApplication.run(ConsumerServiceApplication.class, args);
 	}
 
